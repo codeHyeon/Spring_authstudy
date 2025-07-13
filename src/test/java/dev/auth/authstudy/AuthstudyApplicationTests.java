35807845bb46290e191ccf9287c3f5f39f75867e
hyeon-seo <rrkdgustj727@gmail.com>
2025-07-13 14:54:31 +0900

    Initial commit

---
+++ src/test/java/dev/auth/authstudy/AuthstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.auth.authstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
